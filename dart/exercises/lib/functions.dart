b9f7267faa475f8282b527f881f60135f8ddca8c
Vinh Nguyen <zinh@users.noreply.github.com>
2021-05-08 15:33:13 +0900

    update

--- dart/exercises/lib/functions.dart
+++ dart/exercises/lib/functions.dart
@@ -2,10 +2,20 @@ int add(int a, int b) {
   return a + b;
 }
 
-List<List<int>> halve(List<int> lst) {
+List<List<T>> halve<T>(List<T> lst) {
   var l = lst.length;
   if (l % 2 != 0)
     throw ArgumentError('list should have even length');
   final m = l ~/ 2;
   return [lst.take(m).toList(), lst.skip(m).toList()];
+}
+
+T third<T>(List<T> lst) {
+  if (lst.length < 3)
+    throw ArgumentError('list should have more than 3 elements');
+  return lst[2];
+}
+
+bool luhn(String card) {
+  return false;
 }
--- dart/exercises/test/exercises_test.dart
+++ dart/exercises/test/exercises_test.dart
@@ -11,4 +11,11 @@ void main() {
     expect(halve([1,2,3,4]), equals([[1,2], [3,4]]));
     expect(() => halve([1,2,3]), throwsArgumentError);
   });
+
+  test('third', () {
+    expect(third([1,2,3]), equals(3));
+    expect(third([1,2]), throwsArgumentError);
+    expect(third([1]), throwsArgumentError);
+    expect(third([]), throwsArgumentError);
+  });
 }
