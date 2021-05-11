4ef1bd9b9a3dc4050514818f3f38e44ec017fa56
Vinh Nguyen <zinh@users.noreply.github.com>
2021-05-11 15:19:14 +0900

    version 2

--- python/longest_substring.py
+++ python/longest_substring.py
@@ -1,4 +1,4 @@
-def longest_substring(s: str) -> int:
+def longest_substringv1(s: str) -> int:
     pos : int = 0
     max_len : int = 0
     current_substring : list[str] = []
@@ -17,3 +17,18 @@ def longest_substring(s: str) -> int:
                 max_len = len(current_substring)
         pos += 1
     return len(current_substring)
+
+def longest_substring(s: str) -> int:
+    chars = [0] * 128
+    left = right = 0
+    res = 0
+    while right < len(s):
+        r = s[right]
+        chars[ord(r)] += 1
+        while chars[ord(r)] > 1:
+            l = s[left]
+            chars[ord(l)] -= 1
+            left += 1
+        res = max(res, right - left + 1)
+        right += 1
+    return res
