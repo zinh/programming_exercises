9a90b891bbc0c8afab74cd9a4ac33e33b775f1f5
User Vinh <vinh@www491.sakura.ne.jp>
2021-11-14 16:22:08 +0900

    add files

--- rosalind/dna/rust/src/main.rs
+++ rosalind/dna/rust/src/main.rs
@@ -1,10 +1,17 @@
+extern crate clap;
 use std::io;
 use std::io::Read;
-use std::env;
+use clap::{App,Arg};
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let matches = App::new("Sample App")
+        .arg(Arg::with_name("input file")
+             .short("f")
+             .long("file")
+             .value_name("FILE")
+             .takes_value(true))
+        .get_matches();
+    println!("{:?}", matches);
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
     println!("{}", buffer);
